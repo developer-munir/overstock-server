bb1f4273d1d182d88eafb0b60dbfd4b4c0525a77
developer-munir <devolopermunir@gmail.com>
2022-11-24 12:46:37 +0600

    setup database

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('overstock server is running on server');
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.asxi1ae.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("overstock server is running on server");
+});
 app.listen(port, () => {
-    console.log(`app running on port: ${port}`)
-})
+  console.log(`app running on port: ${port}`);
+});
