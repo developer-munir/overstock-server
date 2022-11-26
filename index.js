5201de6138243ff0cdd7a6cee14f6ac2fb36b2b9
developer-munir <devolopermunir@gmail.com>
2022-11-26 20:53:24 +0600

    create jwt token and set admin role

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.asxi1ae.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -17,48 +17,75 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
+    if (error) {
+      return res.status(403).send("forbiden access");
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
-  const userCollection = client.db('overstockDB').collection('users');
-  const categoriesCollection = client.db('overstockDB').collection('categories');
-  const bookingCollection = client.db('overstockDB').collection('bookings');
+  const userCollection = client.db("overstockDB").collection("users");
+  const categoriesCollection = client
+    .db("overstockDB")
+    .collection("categories");
+  const bookingCollection = client.db("overstockDB").collection("bookings");
   try {
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
-    })
-    app.get('/users', async(req,res)=>{
+    });
+    app.get("/users", async (req, res) => {
       const query = {};
       const result = await userCollection.find(query).toArray();
       res.send(result);
-    })
-    app.get('/categories', async (req, res) => {
+    });
+    app.get("/categories", async (req, res) => {
       const query = {};
       const result = await categoriesCollection
         .find(query)
-        .project({ category_name: 1, category_id:1,picture:1,details:1})
+        .project({ category_name: 1, category_id: 1, picture: 1, details: 1 })
         .limit(4)
         .toArray();
       res.send(result);
-    })
+    });
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { category_id: id};
+      const query = { category_id: id };
       const result = await categoriesCollection.find(query).toArray();
       res.send(result);
     });
-    app.post('/bookings', async (req, res) => {
+    app.post("/bookings", async (req, res) => {
       const bookings = req.body;
-      console.log(bookings)
+      console.log(bookings);
       const result = await bookingCollection.insertOne(bookings);
       res.send(result);
+    });
+    app.put('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
     })
-  } 
-  finally {
-    
+  } finally {
   }
 }
-run().catch(error => console.log(error))
+run().catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("overstock server is running on server");
